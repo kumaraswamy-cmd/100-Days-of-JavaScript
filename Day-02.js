d04069173eb4e00f9fba03757c4816844893cdba
Kumar <kumaraswamythale@gmail.com>
2026-06-01 17:37:56 +0530

    Day-02 Variables

---
+++ Day-02.js
@@ -0,0 +1,41 @@
+//VARIABLES
+//DECLARATION AND ASSIGNMENT
+
+/*Easy 🟢
+
+1. Personal Info
+Create variables for:
+
+Your name
+Age
+College
+
+Print them in a sentence.
+
+Example output: My name is Kumar, I am 20 years old and I study at XYZ College.*/
+
+
+let name = "Kumar";
+let Age = 20;
+let College = "MVGR College of Engineering";
+
+
+console.log(`My name is ${name} , Im ${Age} years old and I study at ${College}.`);
+
+
+
+
+/*2. Favorite Things
+Store:
+
+Favorite movie
+Favorite food
+Favorite game
+
+Print them.*/
+
+let FavMovie = "Interstellar";
+let FavFood = "Biriyani";
+let FavGame = "Cricket";
+
+console.log(`My Favorite Movie is ${FavMovie}, My Favorite Food is ${FavFood} and My Favorite Game is ${FavGame}.`);
