1eb9d7f692ffefe608fc0bb640dac812496118c9
Kumar <kumaraswamythale@gmail.com>
2026-06-05 20:28:26 +0530

    Day-04 Variables

--- Day-04.js
+++ Day-04.js
@@ -10,11 +10,6 @@ console.log(`Im learning JavaScript`);
 window.alert("Hello Kumar");
 window.alert("I Like Biriyani");
 
-/*this is a 
-multi line comment
-*/
-
-// This is a single line comment
 
 document.getElementById("myId").textContent = `Hello Kumarr`;
 document.getElementById("myP").textContent = `I Like JavaScript`;
