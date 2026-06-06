79a23899540c1a75d8ad608206dfdfaa376b6e12
Kumar <kumaraswamythale@gmail.com>
2026-06-06 18:44:48 +0530

    Day-06 Basic Math

--- Day-06.js
+++ Day-06.js
@@ -12,3 +12,34 @@ console.log(multyply);
 
 var divide = 108 / 7 ;
 console.log(divide);
+
+
+//there are two types of increament 
+
+var myVar = 26;
+myVar = myVar + 1 ;
+console.log(myVar);
+
+
+
+var myVar = 48;
+myVar++;
+console.log(myVar);
+
+//decreament
+myVar--;
+console.log(myVar);
+
+
+//decimal
+
+var ourDecimal = 5.7 ;
+console.log(ourDecimal);
+
+
+//basic math is same like integers + , - , * 
+
+
+var remainder;
+remainder = 11 % 3 ;
+console.log(remainder);
