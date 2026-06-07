c9c8de0c3a9fa2fc7d5a6125bded35b3ea74feb6
Kumar <kumaraswamythale@gmail.com>
2026-06-07 20:00:11 +0530

    Day-07 Strings

---
+++ Day-07.js
@@ -0,0 +1,29 @@
+ // concatenating strings 
+
+let myName = "Kumar" ;
+let myStr = " Hello, my Name is " + myName + ". how are you ?";
+
+console.log(myStr);
+
+
+
+//length of the  string 
+
+let firstname = "Kumar";
+let firstnamelength = firstname.length ;
+
+console.log(firstnamelength);
+
+let lastname = `Swamy`;
+let lastnamelength = lastname.lengthco ;
+console.log(lastnamelength);
+
+
+console.log("my name is "+ firstname + lastname);
+
+let fullname = firstname + lastname ;
+
+let fullnamelength = fullname.length;
+
+console.log("my  full name is " + fullname);
+console.log("my full name length is " + fullnamelength);
