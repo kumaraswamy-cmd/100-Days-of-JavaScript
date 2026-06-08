a9deb9e1cc01cbaba0f5914ec70fe08241fb2afb
Kumar <kumaraswamythale@gmail.com>
2026-06-08 20:46:16 +0530

    Day-08 Arrays

--- Day-08.js
+++ Day-08.js
@@ -1,1 +1,10 @@
-console.log('Day-08');
+//arrays
+let ourArray = [["kumar" , 20] , ["Hanish , 9"] , ["Harshad", 14]];
+console.log(ourArray);
+ 
+
+// indexing is possible in arrays
+var firstArray = ourArray[0];
+console.log(firstArray)
+
+
