cecf0dc7895acc57c0f88dbb0172307ad8f42f91
Kumar <kumaraswamythale@gmail.com>
2026-06-09 19:55:52 +0530

    Day-09 Arrays

---
+++ Day-09.js
@@ -0,0 +1,23 @@
+// we can charge elements in array using indexing 
+
+let myarray = [18 , 90 , 300];
+
+myarray[0] = 69 ;
+
+console.log(myarray);
+
+
+
+//multidimentional Arrays 
+
+let myArray = [[1,2,3] , [4 , 5,6] , [5, 23, 34] , [[4 , 5, 6, ] , 55 , 76 ]];
+
+let mydata = myArray[2][1];
+
+console.log(mydata);
+
+
+//  manipulating arrays 
+
+myArray.push([1 , 2, 3 ]);
+console.log(myArray);
