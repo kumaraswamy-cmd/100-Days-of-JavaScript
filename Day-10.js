3d45c8f2a8128b96e5ede803c8af33234652f261
Kumar <kumaraswamythale@gmail.com>
2026-06-10 22:35:38 +0530

    Day-10 Arrays

--- Day-10.js
+++ Day-10.js
@@ -1,1 +1,17 @@
-console.log("Initial commit");
+//using pop
+
+let ourarray = [1, 2, 4, 4 , 55 ];
+let removedfromarray = ourarray.pop();
+console.log(ourarray);
+console.log(removedfromarray);
+
+//using shift
+let firstremovedarray = ourarray.shift();
+console.log(firstremovedarray);
+
+//using unshift 
+ourarray.unshift("Kumar");
+console.log(ourarray);
+
+
+
